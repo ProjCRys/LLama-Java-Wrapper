fa75a6ac94899d4ef31086009ff61a3ff97b099b
ProjCRys <127005727+ProjCRys@users.noreply.github.com>
2023-05-21 20:56:44 +0800

    Update run.bat

--- Project_Arufu/llama-master-63d2046-bin-win-avx-x64/run.bat
+++ Project_Arufu/llama-master-63d2046-bin-win-avx-x64/run.bat
@@ -4,7 +4,7 @@ cd /d "%~dp0"
 title llama.cpp
 
 set "projectFolderPath=%~dp0..\..\"
-set "mainExePath=%projectFolderPath%\Pygmalion\llama-master-63d2046-bin-win-avx-x64\main.exe"
+set "mainExePath=%projectFolderPath%\Project_Arufu\llama-master-63d2046-bin-win-avx-x64\main.exe"
 
 start "" /B /WAIT "%mainExePath%" -i --interactive-first -r "### Human:" --temp 0 -c 2048 -n -1 --ignore-eos --repeat_penalty 1.2 --instruct -m ggml-model-q4_0.bin
 
